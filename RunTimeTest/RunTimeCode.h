2a3c32084e18edecf6dd81940c133aa4995fa5c6
chebao <chebao@chebaodeimac.local>
2018-01-09 17:28:35 +0800

    com

---
+++ RunTimeTest/RunTimeCode.h
@@ -0,0 +1,26 @@
+//
+//  RunTimeCode.h
+//  RunTimeTest
+//
+//  Created by chebao on 2018/1/9.
+//  Copyright © 2018年 jirui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <objc/runtime.h>
+#import "Person.h"
+
+
+//一、动态添加一个类
+
+/*二、通过runtime获取一个类的所有属性，我们可以做些什么？
+ 1. 打印一个类的所有ivar, property 和 method（简单直接的使用）
+ 2. 动态变量控制
+ 3. 在NSObject的分类中增加方法来避免使用KVC赋值的时候出现崩溃
+ */
+
+
+@interface RunTimeCode : NSObject
++(void)chuangJanYiGeLei;
+
+@end
--- RunTimeTest/ViewController.m
+++ RunTimeTest/ViewController.m
@@ -10,15 +10,108 @@
 #import "Person.h"
 #import <objc/message.h>
 #import "NSURL+url.h"
-@interface ViewController ()
+#import "RunTimeCode.h"
 
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
+@property (nonatomic,strong) UITableView * table;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    _table = [[UITableView alloc] init];
+    _table.frame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height);
+    _table.backgroundColor = [UIColor lightGrayColor];
+    _table.delegate = self;
+    _table.dataSource = self;
+    [self.view addSubview:_table];
+
+
     
+    //只是一些临时代码########################################################################################
+//    [self linshi];
+    
+    //潭州的runtime公开课
+//    [self tanzhou];
+    
+}
+
+#pragma mark- table delegate
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 3;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell * cell = [[UITableViewCell alloc] init];
+    return cell;
+}
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    if (indexPath.row == 0) {
+        [RunTimeCode chuangJanYiGeLei];
+    }
+}
+
+#pragma mark- 临时代码的一部分
+-(void)linshi{
+    /*    NSMethodSignature苹果官方定义该类为对方法的参数、返回类似进行封装，
+     协同NSInvocation实现消息转发。
+     通过消息转发实现类似C++中的多重继承。
+     */
+    int a = 1;
+    int b = 2;
+    int c = 3;
+    SEL myMethod = @selector(myLog:param:parm:);
+    SEL myMethod2 = @selector(myLog);
+    // 创建一个函数签名，这个签名可以是任意的，但需要注意，签名函数的参数数量要和调用的一致。
+    NSMethodSignature *sig = [[self class] instanceMethodSignatureForSelector:myMethod];
+    // 通过签名初始化
+    NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:sig];
+    // 2.FirstViewController *view = self;
+    // 2.[invocation setArgument:&view atIndex:0];
+    // 2.[invocation setArgument:&myMethod2 atIndex:1];
+    // 设置target
+    // 1.[invocation setTarget:self];
+    // 设置selector
+    [invocation setSelector:myMethod];
+    // 注意：1、这里设置参数的Index 需要从2开始，因为前两个被selector和target占用。
+    [invocation setArgument:&a atIndex:2];
+    [invocation setArgument:&b atIndex:3];
+    [invocation setArgument:&c atIndex:4];
+    // [invocation retainArguments];
+    // 我们将c的值设置为返回值
+    [invocation setReturnValue:&c];
+    int d;
+    // 取这个返回值
+    [invocation getReturnValue:&d];
+    NSLog(@"d:%d", d);
+    
+    NSUInteger argCount = [sig numberOfArguments];
+    NSLog(@"argCount:%ld", argCount);
+    
+    for (NSInteger i=0; i<argCount; i++) {
+        NSLog(@"%s", [sig getArgumentTypeAtIndex:i]);
+    }
+    NSLog(@"returnType:%s ,returnLen:%ld", [sig methodReturnType], [sig methodReturnLength]);
+    NSLog(@"signature:%@" , sig);
+    
+    // 消息调用
+    [invocation invokeWithTarget:self];
+}
+- (int)myLog:(int)a param:(int)b parm:(int)c
+{
+    NSLog(@"MyLog:%d,%d,%d", a, b, c);
+    return a+b+c;
+}
+
+- (void)myLog
+{
+    NSLog(@"你好,South China University of Technology");
+}
+
+
+#pragma mark- 潭州
+-(void)tanzhou{
     /*
      这部分是潭州的runtime课程
      1 把oc的方法调用使用runtime代码写出来
@@ -37,7 +130,7 @@
      */
     
     
-//    1 把oc的方法调用使用runtime代码写出来
+    //    1 把oc的方法调用使用runtime代码写出来
     Person * p = [[Person alloc] init];
     [p eat];
     [p performSelector:@selector(eat)];
@@ -50,90 +143,18 @@
     objc_msgSend(p, @selector(eat));
     
     
-//    2 使用runtime交换方法的实现，提高容错率
-
+    //    2 使用runtime交换方法的实现，提高容错率
+    
     NSURL * url = [NSURL URLWithString:@"中"];
     NSURLRequest * request = [NSURLRequest requestWithURL:url];
     NSLog(@"%@",request);
     
     
-//    3 动态添加方法  p是没有run这个方法的
-
+    //    3 动态添加方法  p是没有run这个方法的
+    
     [p performSelector:@selector(run)];
-    
-    
-    
-    //下面是网页上总结的一些runtime使用########################################################################################
-    
-    
-    
-     //一、动态添加一个类
-    [self chuangjian];
-
-     /*二、通过runtime获取一个类的所有属性，我们可以做些什么？
-         1. 打印一个类的所有ivar, property 和 method（简单直接的使用）
-         2. 动态变量控制
-         3. 在NSObject的分类中增加方法来避免使用KVC赋值的时候出现崩溃
-     */
-    [self logAllProperty];
-    [self controlProperty];
-    [self voidError];
-    
-    
 }
 
--(void)chuangjian{
-    // 创建一个类(size_t extraBytes该参数通常指定为0, 该参数是分配给类和元类对象尾部的索引ivars的字节数。)
-    Class clazz = objc_allocateClassPair([NSObject class], "GoodPerson", 0);
-    
-    // 添加ivar
-    // @encode(aType) : 返回该类型的C字符串
-    class_addIvar(clazz,"_name", sizeof(NSString *), log2(sizeof(NSString *)), @encode(NSString *));
-    class_addIvar(clazz, "_age", sizeof(NSUInteger), log2(sizeof(NSUInteger)), @encode(NSUInteger));
-    
-    
-    // 注册该类
-    objc_registerClassPair(clazz);
-    
-    // 创建实例对象
-    id object = [[clazz alloc] init];
-    
-    // 设置ivar
-    [object setValue:@"Tracy" forKey:@"name"];
-    
-    Ivar ageIvar = class_getInstanceVariable(clazz, "_age");
-    object_setIvar(object, ageIvar, @18);
-    
-    // 打印对象的类和内存地址
-    NSLog(@"%@", object);
-    
-    // 打印对象的属性值
-    NSLog(@"name = %@, age = %@", [object valueForKey:@"name"], object_getIvar(object, ageIvar));
-    
-    // 当类或者它的子类的实例还存在，则不能调用objc_disposeClassPair方法
-    object = nil;
-    
-    // 销毁类
-    objc_disposeClassPair(clazz);
-}
-
--(void)logAllProperty{
-    Person * p = [[Person alloc] init];
-    [p setValue:@"Kobe" forKey:@"name"];
-    [p setValue:@18 forKey:@"age"];
-    p.weight = 110.0f;
-    
-    //1.打印所有ivars
-    unsigned int ivarCount = 0;
-    
-}
-
--(void)controlProperty{
-    
-}
--(void)voidError{
-    
-}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
